d3a2928f6bf1ecd310875866d046eba57abcf7e3
NERO <mxzxro@gmail.com>
2024-07-20 14:28:24 +0900

    🔧 SceneDelegate RootVC 설정

---
+++ TALK-CADEMY/ChatListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ChatListViewController.swift
+//  TALK-CADEMY
+//
+//  Created by NERO on 7/19/24.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
